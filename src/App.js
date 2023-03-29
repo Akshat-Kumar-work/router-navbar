7195ea1dcfe6a8c7cd5b2ac6d1ef9eb215772b22
Akshat <iamakku0.0.0.1@gmail.com>
2023-03-29 17:32:30 +0530

    commiting on 29 march 23

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home"
 function App() {
   return (
     <div>
+          <NavBar></NavBar>
 
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+          </Routes>
     </div>
   );
 }
