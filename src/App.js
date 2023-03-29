21da6f4ef43cfa8989a704faa527a04d089279fc
Akshat <iamakku0.0.0.1@gmail.com>
2023-03-29 18:32:11 +0530

    commiting on 29 march 23

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
 function App() {
   return (
     <div>
           <NavBar></NavBar>
 
           <Routes>
-            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/" element={<Home></Home>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="signup" element={<Signup/>}/>
+            <Route path="dashboard" element={<Dashboard/>}/>
           </Routes>
     </div>
   );
